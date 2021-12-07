8b4137c69d004edcbaf9b1966d625f6782288dfd
Sigaretif <sigaretif@gmail.com>
2021-12-07 11:50:26 +0100

    popup podczas dodawania nowego elementu & reset

--- app/src/main/java/com/example/pack_man/ExpandableListActivity.java
+++ app/src/main/java/com/example/pack_man/ExpandableListActivity.java
@@ -52,15 +52,48 @@ public class ExpandableListActivity extends Activity {
                                         int groupPosition, int childPosition, long id) {
 
                 TextView count = v.findViewById(R.id.ListItemCount);
+                TextView name = v.findViewById(R.id.lblListItem);
                 MyPair child = (MyPair) listAdapter.getChild(groupPosition, childPosition);
 
                 if (listAdapter.getChildId(groupPosition, childPosition) + 1 == listAdapter.getChildrenCount(groupPosition)) {
                 //if (childPosition + 1 == listAdapter.getChildrenCount(groupPosition)) {
                     MyPair newElement = new MyPair("Dodaj nowy element", "+");
 
-                    BasicList.getGroupList(groupPosition).add(newElement);
-                    recreate();
-                } else {
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                    View popupView = inflater.inflate(R.layout.popup_on_last_list_click, null);
+                    EditText newCount = popupView.findViewById(R.id.newCountID);
+                    newCount.setText("");
+                    EditText newName = popupView.findViewById(R.id.newNameID);
+                    newName.setText("");
+                    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    //int width = 600;
+                    //int height = 800;
+                    boolean focusable = true;
+                    final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                    popupWindow.setElevation(50);
+                    popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                    Button popupButton = popupView.findViewById(R.id.applyChangesID);
+                    popupButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            EditText inputline1 = popupView.findViewById(R.id.newCountID);
+                            String newCount = inputline1.getText().toString();
+                            EditText inputline2 = popupView.findViewById(R.id.newNameID);
+                            String newName = inputline2.getText().toString();
+                            child.setCount(newCount);
+                            count.setText(child.getCount());
+                            child.setName(newName);
+                            name.setText(child.getName());
+                            popupWindow.dismiss();
+                            BasicList.getGroupList(groupPosition).add(newElement);
+                            recreate();
+                        }
+                    });
+//                    BasicList.getGroupList(groupPosition).add(newElement);
+//                    recreate();
+                }
+                else {
                     LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
                     View popupView = inflater.inflate(R.layout.popup_on_list_click, null);
                     EditText newCount = popupView.findViewById(R.id.newCountID);
@@ -93,6 +126,18 @@ public class ExpandableListActivity extends Activity {
     private String getPreference(String key) {
         SharedPreferences sharedPref = getSharedPreferences("GLOBAL_PREFERENCES", Context.MODE_PRIVATE);
         return sharedPref.getString(key, null);
+    }
+
+    public void resetExpandableList(View view){
+        BasicList.listAlreadySet = false;
+        BasicList.mapAlreadySet = false;
+        recreate();
+//        listDataHeader = BasicList.getListDataHeader();
+//        listDataChild = BasicList.getListDataChild(tripLength);
+//
+//        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+//        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+//        expListView.setAdapter(listAdapter);
     }
 
     public void goToListTesting(View view){
